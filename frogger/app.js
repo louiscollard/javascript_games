721b68e50a382ec41e4ef28b93d2f7cdad6bcdc9
Louis Collard <louiscollard2@gmail.com>
2022-04-03 23:57:42 +0200

    Add frogs and cars

---
+++ frogger/app.js
@@ -0,0 +1,34 @@
+const timeLeftDisplay = document.querySelector("#time-left");
+const resultDisplay = document.querySelector("#result");
+const startPauseButton = document.querySelector("#start-pause-button");
+const squares = document.querySelectorAll(".grid div");
+console.log(squares);
+let currentIndex = 76;
+let width = 9;
+
+function moveFrog(e) {
+	squares[currentIndex].classList.remove("frog");
+
+	switch (e.key) {
+		case "ArrowLeft":
+			console.log("move left");
+			if (currentIndex % width !== 0) currentIndex -= 1;
+			break;
+		case "ArrowRight":
+			console.log("move right");
+			currentIndex += 1;
+			break;
+		case "ArrowUp":
+			console.log("move up");
+			currentIndex -= width;
+			break;
+		case "ArrowDown":
+			console.log("move down");
+			currentIndex += width;
+			break;
+	}
+
+	squares[currentIndex].classList.add("frog");
+}
+
+document.addEventListener("keyup", moveFrog);
